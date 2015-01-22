c6698ecb0cc895f735f3544b74dc5bf26accc6e3
langz <anderslangseth@hotmail.com>
2015-01-22 17:07:34 +0100

    More on discover

--- app/scripts/controllers/discover.js
+++ app/scripts/controllers/discover.js
@@ -8,9 +8,17 @@
 * Controller of the vgListaVizApp
 */
 angular.module('vgListaVizApp')
-.controller('DiscoverCtrl', function ($scope, songs) {
-  $scope.songs = {};
+.controller('DiscoverCtrl', function ($scope, songs, charts, summaryArtist) {
+  $scope.songs = [];
+  $scope.artists = [];
+  $scope.charts = [];
   $scope.safeToChange=false;
+
+  $scope.types = [
+  'Artist',
+  'Chart',
+  'Song'
+  ];
 
   $scope.itemX = {
     name:'danceability',
@@ -19,56 +27,6 @@ angular.module('vgListaVizApp')
     max:1
   };
   $scope.choicesX = [
-  {
-    name:'danceability',
-    value:0,
-    min:0,
-    max:1
-  },
-  {
-    name:'duration',
-    value:1,
-    min:0,
-    max:1850
-  },
-  {
-    name:'energy',
-    value:2,
-    min:0,
-    max:1
-  },
-  {
-    name:'loudness',
-    value:4,
-    min:-55.5,
-    max:0
-  },
-  {
-    name:'mode',
-    value:5,
-    min:0, max:1
-  },
-  {
-    name:'tempo',
-    value:6,
-    min:0,
-    max:225
-  },
-  {
-    name:'timesignature',
-    value:7,
-    min:1,
-    max:7
-  }
-  ];
-
-  $scope.itemY = {
-    name:'duration',
-    value:1,
-    min:0,
-    max:1850
-  };
-  $scope.choicesY = [
 {
   name:'danceability',
   value:0,
@@ -110,33 +68,181 @@ angular.module('vgListaVizApp')
   min:1,
   max:7
 }
-  ];
+];
 
-  songs.query( {soundSummary: {$not: {$size: 0}}}, {limit:100000}).then(function(res){
-    $scope.songs = res;
-    console.log(res);
-    $scope.update($scope.itemX, $scope.itemY);
+$scope.itemY = {
+  name:'duration',
+  value:1,
+  min:0,
+  max:1850
+};
+$scope.choicesY = [
+{
+  name:'danceability',
+  value:0,
+  min:0,
+  max:1
+},
+{
+  name:'duration',
+  value:1,
+  min:0,
+  max:1850
+},
+{
+  name:'energy',
+  value:2,
+  min:0,
+  max:1
+},
+{
+  name:'loudness',
+  value:4,
+  min:-55.5,
+  max:0
+},
+{
+  name:'mode',
+  value:5,
+  min:0, max:1
+},
+{
+  name:'tempo',
+  value:6,
+  min:0,
+  max:225
+},
+{
+  name:'timesignature',
+  value:7,
+  min:1,
+  max:7
+}
+];
+//INIT
+songs.query( {soundSummary: {$not: {$size: 0}}}, {limit:1}).then(function(res){
+  $scope.songs = res;
+  console.log(res.length);
+  $scope.update($scope.itemX, $scope.itemY, $scope.songs);
+  $scope.safeToChange=true;
+});
+
+$scope.getSongs = function(){
+  if($scope.songs.length===0){
+    songs.query( {soundSummary: {$not: {$size: 0}}}, {limit:1}).then(function(res){
+      $scope.songs = res;
+      $scope.update($scope.itemX, $scope.itemY, $scope.songs);
+      $scope.try.loading = false;
+      $scope.safeToChange=true;
+    });
+
+  }
+  else{
+    $scope.try.loading = false;
     $scope.safeToChange=true;
-  });
+    console.log('finnes songs');
+    $scope.update($scope.itemX, $scope.itemY, $scope.songs);
+
+  }
+};
+$scope.getCharts = function(){
+  if($scope.charts.length===0){
+
+    charts.query( {soundSummary: {$not: {$size: 0}}}, {limit:1}).then(function(res){
+      $scope.charts = res;
+      $scope.update($scope.itemX, $scope.itemY, $scope.charts);
+      $scope.try.loading = false;
+      $scope.safeToChange=true;
+    });
+
+  }
+  else{
+    $scope.try.loading = false;
+    $scope.safeToChange=true;
+    console.log('finnes charts');
+    $scope.update($scope.itemX, $scope.itemY, $scope.charts);
+
+  }
+};
+$scope.getArtists = function(){
+
+  if($scope.artists.length===0){
+    summaryArtist.query( { danceability: { $exists: true, $nin: [ 0 ] } }, {limit:1}).then(function(res){
+      $scope.artists = res;
+      $scope.update($scope.itemX, $scope.itemY, $scope.artists);
+      $scope.try.loading = false;
+      $scope.safeToChange=true;
+    });
+  }
+  else{
+    $scope.try.loading = false;
+    $scope.safeToChange=true;
+    console.log('finnes artists');
+    $scope.update($scope.itemX, $scope.itemY, $scope.artists);
+  }
+};
 
 
+$scope.changeType = function(){
+$scope.try.loading = true;
+  $scope.safeToChange=false;
+  console.log('halla');
+  if($scope.type==='Artist'){
+    $scope.getArtists();
+  }
+  else if($scope.type==='Chart'){
+    $scope.getCharts();
+  }
+  else if($scope.type==='Song'){
+    $scope.getSongs();
 
-  $scope.update = function(xaxis, yaxis){
-    $scope.produceData(xaxis, yaxis);
+  }
+  else{
+
+  }
+  $scope.safeToChange=true;
+};
 
 
-  };
-  $scope.produceData = function(xaxis, yaxis){
-    $scope.try.xAxis.title.text = xaxis.name;
-    $scope.try.yAxis.title.text = yaxis.name;
+$scope.update = function(xaxis, yaxis, datainp){
 
-    $scope.safeToChange=false;
-    $scope.try.series[0].data=[];
-    for(var i = 0; i<$scope.songs.length; i ++){
-      var sangen = $scope.songs[i];
+  $scope.produceData(xaxis, yaxis, datainp);
 
 
-      var data = {
+};
+$scope.produceData = function(xaxis, yaxis, datainp){
+console.log('produsererer nå');
+  $scope.try.loading = true;
+  $scope.try.xAxis.title.text = xaxis.name;
+  $scope.try.yAxis.title.text = yaxis.name;
+
+  $scope.safeToChange=false;
+  $scope.try.series[0].data=[];
+  for(var i = 0; i<datainp.length; i ++){
+
+    var sangen = datainp[i];
+    var data = {};
+    if($scope.type==='Chart'){
+      data = {
+        name:sangen.year,
+        x:sangen.soundSummary[xaxis.value][xaxis.name],
+        y:sangen.soundSummary[yaxis.value][yaxis.name],
+        artist:sangen.week,
+        xtype: xaxis.name,
+        ytype:yaxis.name
+      }
+    }
+    else if($scope.type==='Artist'){
+      data = {
+        name:sangen.artist,
+        x:sangen[xaxis.name],
+        y:sangen[yaxis.name],
+        xtype: xaxis.name,
+        ytype:yaxis.name
+      }
+    }
+    else if($scope.type==='Song'){
+      data = {
         name:sangen.title,
         x:sangen.soundSummary[xaxis.value][xaxis.name],
         y:sangen.soundSummary[yaxis.value][yaxis.name],
@@ -144,86 +250,88 @@ angular.module('vgListaVizApp')
         xtype: xaxis.name,
         ytype:yaxis.name
       }
-      $scope.try.series[0].data.push(data);
     }
+    $scope.try.series[0].data.push(data);
+  }
 
 
-    $scope.try.loading = false;
-    $scope.safeToChange=true;
-  };
+  $scope.try.loading = false;
+  $scope.safeToChange=true;
+};
 
 
 
-  $scope.try = {
-    options: {
-      chart: {
-        type: 'scatter',
-        zoomType: 'xy'
-      },
-      title: {
-        text: 'Discovery'
-      },
+$scope.try = {
+  options: {
+    chart: {
+      type: 'scatter',
+      zoomType: 'xy'
+    },
+    title: {
+      text: ''
+    },
 
-      plotOptions: {
-        scatter: {
-          marker: {
-            radius: 5,
-            states: {
-              hover: {
-                enabled: true,
-                lineColor: 'rgb(100,100,100)'
-              }
-            }
-          },
+    plotOptions: {
+      scatter: {
+        marker: {
+          radius: 5,
           states: {
             hover: {
-              marker: {
-                enabled: false
-              }
+              enabled: true,
+              lineColor: 'rgb(100,100,100)'
             }
-          },
-          tooltip: {
-            headerFormat:'',
-            pointFormat: '<b>{point.name}</b> by {point.artist} <br/>{point.xtype}: {point.x:.1f} <br/> {point.ytype}: {point.y:.1f} '
           }
         },
-        series: {
-          turboThreshold: 4000
+        states: {
+          hover: {
+            marker: {
+              enabled: false
+            }
+          }
+        },
+        tooltip: {
+          headerFormat:'',
+          pointFormat: '<b>{point.name}</b>  {point.artist} <br/>{point.ytype}: {point.y:.1f} <br/> {point.xtype}: {point.x:.1f} '
         }
-      }
-    },
-    xAxis: {
-      title: {
-        enabled: true,
-        text: 'xAxis'
       },
-      startOnTick: true,
-      endOnTick: true,
-      showLastLabel: true
-    },
-    yAxis: {
-      title: {
-        text: 'yAxis'
+      series: {
+        turboThreshold: 4000
       }
+    }
+  },
+  xAxis: {
+    title: {
+      enabled: true,
+      text: 'xAxis'
     },
-    legend: {
-      layout: 'vertical',
-      align: 'left',
-      verticalAlign: 'top',
-      x: 100,
-      y: 70,
-      floating: true,
-      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
-      borderWidth: 1
-    },
-    series: [{
-      showInLegend: false,
-      name: 'Data',
-      color: 'rgba(223, 83, 83, .5)',
-      data: []
+    startOnTick: true,
+    endOnTick: true,
+    showLastLabel: true
+  },
+  yAxis: {
+    title: {
+      text: 'yAxis'
+    }
+  },
+  legend: {
+    layout: 'vertical',
+    align: 'left',
+    verticalAlign: 'top',
+    x: 100,
+    y: 70,
+    floating: true,
+    backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+    borderWidth: 1
+  },
+  series: [{
+    showInLegend: false,
+    name: 'Data',
+    color: 'rgba(223, 83, 83, .5)',
+    data: []
 
-    }],
+  }],
 
-    loading: true
-  }
+  loading: true
+}
+
 });
