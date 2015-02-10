f7e384ae38b3de027e876f375cc56201df911654
langz <anderslangseth@hotmail.com>
2015-02-10 17:42:55 +0100

    Added sort by year

--- app/scripts/controllers/discover.js
+++ app/scripts/controllers/discover.js
@@ -15,112 +15,103 @@ angular.module('vgListaVizApp')
   $scope.safeToChange=false;
 
   $scope.types = [
-  'Artist',
-  'Liste',
-  'Sang'
+    'Artist',
+    'Liste',
+    'Sang'
   ];
 
 
-  var standardX =  [
-  {
-    norsk:'Dansbarhet',
-    name:'danceability',
-    value:0
-  },
-  {
-    norsk:'Energi',
-    name:'energy',
-    value:2
-  },
-  {
-    norsk:'Lydstyrke',
-    name:'loudness',
-    value:4
-  },
-  {
-    norsk:'Modal skala',
-    name:'mode',
-    value:5
-  },
-  {
-    norsk:'Taktart',
-    name:'timesignature',
-    value:7
-  },
-  {
-    norsk:'Tempo',
-    name:'tempo',
-    value:6
-  },
-  {
-    norsk:'Varighet',
-    name:'duration',
-    value:1
-  }
+  var standard =  [
+    {
+      norsk:'Dansbarhet',
+      name:'danceability',
+      value:0,
+      max:1
+    },
+    {
+      norsk:'Energi',
+      name:'energy',
+      value:2,
+      max:1
+    },
+    {
+      norsk:'Lydstyrke',
+      name:'loudness',
+      value:4
+    },
+    {
+      norsk:'Modal skala',
+      name:'mode',
+      value:5,
+      max:1
+    },
+    {
+      norsk:'Taktart',
+      name:'timesignature',
+      value:7,
+      max:7
+    },
+    {
+      norsk:'Tempo',
+      name:'tempo',
+      value:6,
+      max:220
+    },
+    {
+      norsk:'Varighet',
+      name:'duration',
+      value:1,
+      max:1000
+    }
   ];
-  $scope.choicesX = angular.copy(standardX);
-  var standardY = [
-  {
-    norsk:'Dansbarhet',
-    name:'danceability',
-    value:0
-  },
-  {
-    norsk:'Energi',
-    name:'energy',
-    value:2
-  },
-  {
-    norsk:'Lydstyrke',
-    name:'loudness',
-    value:4
-  },
-  {
-    norsk:'Modal skala',
-    name:'mode',
-    value:5
-  },
-  {
-    norsk:'Taktart',
-    name:'timesignature',
-    value:7
-  },
-  {
-    norsk:'Tempo',
-    name:'tempo',
-    value:6
-  },
-  {
-    norsk:'Varighet',
-    name:'duration',
-    value:1
-  }
-  ];
-  $scope.choicesY = angular.copy(standardY);
+
+
+  $scope.choicesX = angular.copy(standard);
+  $scope.choicesY = angular.copy(standard);
   $scope.itemY ={
     norsk:'Dansbarhet',
     name:'danceability',
-    value:0
+    value:0,
+    max:1
   }
   $scope.itemX ={
     norsk:'Energi',
     name:'energy',
-    value:2
+    value:2,
+    max:1
   }
   //INIT
   $scope.choicesY.push({norsk:'Uker på listen', name:'antall'});
   $scope.choicesY.push({norsk:'Beste plassering', name:'bestPos'})
   $scope.choicesX.push({norsk:'Uker på listen', name:'antall'});
   $scope.choicesX.push({norsk:'Beste plassering', name:'bestPos'})
+  $scope.type ='Sang';
   songs.query( {soundSummary: {$not: {$size: 0}}}, {limit:100}).then(function(res){
     $scope.songs = res;
     $scope.update($scope.itemX, $scope.itemY, $scope.songs);
     $scope.safeToChange=true;
   });
 
-  $scope.getSongs = function(){
+  $scope.changeRangeYear = function(yr){
+    console.log($scope.type);
+    if($scope.type==='Liste'){
+
+      $scope.getCharts(yr);
+    }
+    else if($scope.type==='Sang'){
+      $scope.getSongs(yr);
+
+    }
+    else{
+      $scope.getArtists(yr);
+    }
+
+  }
+
+  $scope.getSongs = function(yr){
     if($scope.songs.length===0){
-      songs.query( {soundSummary: {$not: {$size: 0}}}, {limit:100}).then(function(res){
+      songs.query({soundSummary: {$not: {$size: 0}}}, {limit:100}).then(function(res){
+
         $scope.songs = res;
         $scope.update($scope.itemX, $scope.itemY, $scope.songs);
         $scope.try.loading = false;
@@ -134,11 +125,21 @@ angular.module('vgListaVizApp')
       $scope.update($scope.itemX, $scope.itemY, $scope.songs);
 
     }
+    if(yr){
+      var sortData = [];
+      for(var tell = 0 ; tell<$scope.songs.length ; tell ++){
+        console.log($scope.songs[tell].year + " === " + yr)
+        if($scope.songs[tell].years.indexOf(String(yr))){
+          sortData.push($scope.songs[tell]);
+        }
+      }
+      $scope.update($scope.itemX, $scope.itemY, sortData);
+    }
   };
-  $scope.getCharts = function(){
+  $scope.getCharts = function(yr){
     if($scope.charts.length===0){
 
-      charts.query( {soundSummary: {$not: {$size: 0}}}, {limit:100}).then(function(res){
+      charts.query({soundSummary: {$not: {$size: 0}}}, {limit:100}).then(function(res){
         $scope.charts = res;
         $scope.update($scope.itemX, $scope.itemY, $scope.charts);
         $scope.try.loading = false;
@@ -152,11 +153,21 @@ angular.module('vgListaVizApp')
       $scope.update($scope.itemX, $scope.itemY, $scope.charts);
 
     }
+    if(yr){
+      var sortData = [];
+      for(var tell = 0 ; tell<$scope.charts.length ; tell ++){
+        console.log($scope.charts[tell].year + " === " + yr)
+        if($scope.charts[tell].year === (String(yr))){
+          sortData.push($scope.charts[tell]);
+        }
+      }
+      $scope.update($scope.itemX, $scope.itemY, sortData);
+    }
   };
   $scope.getArtists = function(){
 
     if($scope.artists.length===0){
-      summaryArtist.query( { danceability: { $exists: true, $nin: [ 0 ] } }, {limit:1}).then(function(res){
+      summaryArtist.query( { danceability: { $exists: true, $nin: [ 0 ] } }, {limit:100}).then(function(res){
         $scope.artists = res;
         $scope.update($scope.itemX, $scope.itemY, $scope.artists);
         $scope.try.loading = false;
@@ -167,6 +178,16 @@ angular.module('vgListaVizApp')
       $scope.try.loading = false;
       $scope.safeToChange=true;
       $scope.update($scope.itemX, $scope.itemY, $scope.artists);
+    }
+    if(yr){
+      var sortData = [];
+      for(var tell = 0 ; tell<$scope.artists.length ; tell ++){
+        console.log($scope.artists[tell].year + " === " + yr)
+        if($scope.songs[tell].artists.indexOf(String(yr))){
+          sortData.push($scope.artists[tell]);
+        }
+      }
+      $scope.update($scope.itemX, $scope.itemY, sortData);
     }
   };
 
@@ -181,10 +202,10 @@ angular.module('vgListaVizApp')
       $scope.itemY =$scope.choicesY[0];
       $scope.itemX =$scope.choicesX[1];
 
-      $scope.choicesY.push({norsk:'Uker på listen', name:'antall'});
-      $scope.choicesY.push({norsk:'Antall sanger', name:'antallunikesanger'})
-      $scope.choicesX.push({norsk:'Uker på listen', name:'antall'});
-      $scope.choicesX.push({norsk:'Antall sanger', name:'antallunikesanger'})
+      $scope.choicesY.push({norsk:'Uker på listen', name:'antall', max:33});
+      $scope.choicesY.push({norsk:'Antall sanger', name:'antallunikesanger', max:350})
+      $scope.choicesX.push({norsk:'Uker på listen', name:'antall', max:33});
+      $scope.choicesX.push({norsk:'Antall sanger', name:'antallunikesanger', max:350})
 
       $scope.getArtists();
     }
@@ -199,10 +220,10 @@ angular.module('vgListaVizApp')
       $scope.itemY =$scope.choicesY[0];
       $scope.itemX =$scope.choicesX[1];
 
-      $scope.choicesY.push({norsk:'Uker på listen', name:'antall'});
-      $scope.choicesY.push({norsk:'Beste plassering', name:'bestPos'})
-      $scope.choicesX.push({norsk:'Uker på listen', name:'antall'});
-      $scope.choicesX.push({norsk:'Beste plassering', name:'bestPos'})
+      $scope.choicesY.push({norsk:'Uker på listen', name:'antall', max: 60});
+      $scope.choicesY.push({norsk:'Beste plassering', name:'bestPos', max:20})
+      $scope.choicesX.push({norsk:'Uker på listen', name:'antall', max: 60});
+      $scope.choicesX.push({norsk:'Beste plassering', name:'bestPos', max:20})
       $scope.getSongs();
 
     }
@@ -259,6 +280,10 @@ angular.module('vgListaVizApp')
 
   };
   $scope.produceData = function(xaxis, yaxis, datainp){
+
+    $scope.try.xAxis.max = xaxis.max;
+    $scope.try.yAxis.max = yaxis.max;
+
     $scope.try.loading = true;
     $scope.try.xAxis.title.text = xaxis.norsk;
     $scope.try.yAxis.title.text = yaxis.norsk;
@@ -299,16 +324,25 @@ angular.module('vgListaVizApp')
           xtype: xaxis.norsk,
           ytype:yaxis.norsk
         };
-
+        var x = false;
+        var y = false;
         if(xaxis.name==='antall' || xaxis.name ==='bestPos'){
           data.x = sangen[xaxis.name];
+          x = true;
         }
         if(yaxis.name==='antall' || yaxis.name ==='bestPos'){
           data.y = sangen[yaxis.name];
+          y = true;
         }
-        else{
+        if(!x & !y){
           data.x = sangen.soundSummary[xaxis.value][xaxis.name];
           data.y = sangen.soundSummary[yaxis.value][yaxis.name];
+        }
+        if(x & !y){
+          data.y = sangen.soundSummary[yaxis.value][yaxis.name];
+        }
+        if(y & !x){
+          data.x = sangen.soundSummary[xaxis.value][xaxis.name];
         }
 
       }
